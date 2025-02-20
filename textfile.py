1c8352fe65e0007da0c4cc43f12bfdd42a1f4ede
Sid Lalgowdar <sidlalgowdar@Sids-Air.eduroam-wless.du.edu>
2025-02-19 20:35:27 -0700

    idk lmao

--- textfile.py
+++ textfile.py
@@ -60,8 +60,11 @@ while True:
                             view_start -= 1
                             view_end -= 1
                             cursory = 25
-                        cursorx = 48 + len(lines[line_num])*12
                         line_index = len(lines[line_num])
+                        if len(lines[line_num]) > 75:
+                             view_he = len(lines[line_num]) - 75
+                             view_hs = view_he - 75
+                        cursorx = 48 + len(lines[line_num][view_hs:view_he])*12
                     elif line_num == 0:
                         if cursorx < 48:
                             cursorx = 48
@@ -82,9 +85,14 @@ while True:
                         line_index -= 1
                         if cursorx < 48:
                             cursorx = 48
-                            line_index = 0
-            
+                            view_hs -= 1
+                            if view_hs < 0: view_hs = 0
+                            view_he -= 1
+                            if view_he < 75: view_he = 75
+                        if line_index < 0: line_index = 0
+
             elif event.key == pygame.K_RETURN:
+                if rel != 0: rel = 0
                 cursorx = 48
                 cursory += 25
                 lines.insert(line_num+1, lines[line_num][line_index:])
@@ -117,7 +125,7 @@ while True:
                 else:
                     cursorx -= 12
                     line_index -= 1
-                    if cursorx < 48:
+                    if cursorx < 48 and rel==0:
                         cursorx = 48
                         view_hs -= 1
                         if view_hs < 0: view_hs = 0
@@ -164,8 +172,8 @@ while True:
 
             elif rel != 0 and event.key == pygame.K_x:
                 if rel < 0:
-                    clip = lines[line_num][line_index+1:line_index-rel+1]
-                    lines[line_num] = lines[line_num][:line_index+1] + lines[line_num][line_index-rel+1:]
+                    clip = lines[line_num][line_index:line_index-rel+1]
+                    lines[line_num] = lines[line_num][:line_index] + lines[line_num][line_index-rel+1:]
                 elif rel > 0:
                     clip = lines[line_num][line_index-rel:line_index]
                     lines[line_num] = lines[line_num][:line_index-rel] +lines[line_num][line_index:]
@@ -179,7 +187,7 @@ while True:
             
             elif rel != 0 and event.key == pygame.K_c:
                 if rel < 0:
-                    clip = lines[line_num][line_index+1:line_index-rel+1]
+                    clip = lines[line_num][line_index:line_index-rel+1]
                 elif rel > 0:
                     clip = lines[line_num][line_index-rel:line_index]
                 rel = 0
@@ -201,9 +209,18 @@ while True:
                     view_start -= 1
                     view_end -= 1
                 if len(lines[line_num]) < line_index:
-                    cursorx = 48+len(lines[line_num])*12
+                    prev_li = line_index
                     line_index = len(lines[line_num])
-            
+                    viewf = (len(lines[line_num+1])-len(lines[line_num]))
+                    if viewf >= 75 and prev_li-line_index >= 75:
+                        view_he -= viewf
+                        if view_he < 75:
+                            view_hs = 0
+                            view_he = 75
+                        else:
+                            view_hs = view_he - 75
+                    cursorx = 48+len(lines[line_num][view_hs:view_he])*12
+
             elif event.key == pygame.K_DOWN:
                 cursory += 25
                 line_num += 1
@@ -216,8 +233,17 @@ while True:
                     view_start += 1
                     view_end += 1
                 if len(lines[line_num]) < line_index:
-                    cursorx = 48+len(lines[line_num])*12
+                    prev_li = line_index
                     line_index = len(lines[line_num])
+                    viewf = (len(lines[line_num-1])-len(lines[line_num]))
+                    if viewf >= 75 and prev_li-line_index >= 75:
+                        view_he -= viewf
+                        if view_he < 75:
+                            view_hs = 0
+                            view_he = 75
+                        else:
+                            view_hs = view_he - 75
+                    cursorx = 48+len(lines[line_num][view_hs:view_he])*12
             
             elif event.key == pygame.K_TAB:
                 lines[line_num] = lines[line_num][:line_index] + "    " + lines[line_num][line_index:]
@@ -225,6 +251,11 @@ while True:
                     indent += 1
                 line_index += 4
                 cursorx += 12*4
+                if cursorx > 48+75*12:
+                    diff = int((cursorx - (48+75*12))/12)
+                    cursorx = 48+75*12
+                    view_hs += diff
+                    view_he += diff
             
             elif event.key == pygame.K_ESCAPE:
                 sys.exit(0)
@@ -233,11 +264,18 @@ while True:
                 lines[line_num] = lines[line_num][:line_index] + clip + lines[line_num][line_index:]
                 line_index += len(clip)
                 cursorx += 12*len(clip)
+                if cursorx > 48+75*12:
+                    diff = int((cursorx - (48+75*12))/12)
+                    cursorx = 48+75*12
+                    view_hs += diff
+                    view_he += diff
 
             elif (event.key == pygame.K_a) and (event.mod & pygame.KMOD_CTRL):
                 cursorx = 48
                 lines.insert(line_num, "")
                 line_index = 0
+                view_hs = 0
+                view_he = 75
 
             elif (event.key == pygame.K_b) and (event.mod & pygame.KMOD_CTRL):
                 cursorx = 48
@@ -250,12 +288,17 @@ while True:
                     view_start += 1
                     view_end += 1
                 line_index = 0
+                view_hs = 0
+                view_he = 75
 
             elif (event.key == pygame.K_d) and (event.mod & pygame.KMOD_CTRL):
                 if line_num != 0:
                     lines = lines[:line_num] + lines[line_num+1:]
                     if line_num == len(lines):
                         line_num -= 1
+                        if line_num == view_start-1:
+                            view_start -= 1
+                            view_end -= 1
                         cursory -= 25
                         if cursory < 25:
                             cursory = 25
@@ -264,7 +307,10 @@ while True:
                         lines = lines[1:]
                     else:
                         lines[line_num] = ""
-                cursorx = 48 + len(lines[line_num])*12
+                cursorx = 48
+                line_index = 0
+                view_hs = 0
+                view_he = 75
 
             elif event.unicode:
                 lines[line_num] = lines[line_num][:line_index] + event.unicode + lines[line_num][line_index:]
